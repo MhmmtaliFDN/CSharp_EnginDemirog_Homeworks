93900f3f2369e2698808417774dc3ff83a137457
Muhammet Ali Fidan <mhmmtalifdn@gmail.com>
2021-02-21 14:50:39 +0300

    Day 1 - Homework 1

---
+++ Day 1/Day1_Homework1/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Kodlama.io sistemimizde detaylı bir gezinti yapınız.
+Sistemde şart blokları, döngüler ve fonksiyon(metot) olarak kullanıldığını düşündüğünüz kısımları bulunuz. Her biri için en az iki örnek vermelisiniz.*/
+
+using System;
+namespace Day1_Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loops - Döngüler
+            Course course = new Course();
+            course.CourseName = "Yazılım Geliştirici Yetiştirme Kampı";
+            course.TeacherName = "Engin Demiroğ";
+            course.Rate = 40;
+
+            Course[] courses = new Course[] { course };
+
+            foreach (Course item in courses)
+            {
+                Console.WriteLine("Kursun Adı: " + course.CourseName + " - " + "Öğretmenin Adı: " + course.TeacherName + 
+" - " + "Kurs Tamamlama Oranı: " + course.Rate);        //Kursun Adı: Yazılım Geliştirici Yetiştirme Kampı - Öğretmenin Adı: Engin Demiroğ - Kurs Tamamlama Oranı: 40
+            }
+            
+            //Condition and Methods - Koşul ve Metot
+            Control control = new Control();
+            control.HaveYouSıgnedIn("E");               //Sisteme Hoşgeldiniz!
+            control.HaveYouSıgnedIn("H");               //Lütfen Giriş Yapınız!
+        }
+    }
+
+    class Course
+    {
+        public string CourseName { get; set; }
+        public string TeacherName { get; set; }
+        public int Rate { get; set; }
+    }
+
+    class Control
+    {
+        public void HaveYouSıgnedIn(string control)
+        {
+            if (control == "E")
+            {
+                Console.WriteLine("Sisteme Hoşgeldiniz!");
+            }
+            else
+            {
+                Console.WriteLine("Lütfen Giriş Yapınız");
+            }
+        }
+
+    }
+}
